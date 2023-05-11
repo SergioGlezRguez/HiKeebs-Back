432296b64c2f570f41817144da7c19637909bfbb
Sergio Gonzalez Rodriguez <sergioglezrguez06@gmail.com>
2023-05-11 19:57:36 +0100

    Configuracion Inicial (Usando IntelliJ)
    Se ha creado el proyecto con spring initializr y se han puesto las dependencias mas importantes.

---
+++ hikeebs/src/main/java/es/cifpcm/hikeebs/HikeebsApplication.java
@@ -0,0 +1,13 @@
+package es.cifpcm.hikeebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeebsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeebsApplication.class, args);
+	}
+
+}
---
+++ hikeebs/src/test/java/es/cifpcm/hikeebs/HikeebsApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cifpcm.hikeebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikeebsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
